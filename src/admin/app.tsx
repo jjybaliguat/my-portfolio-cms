fa16a684384ed6e0734ad4c5d442ab6e8c399ec2
jjybaliguat <justinejeraldbaliguat@gmai.com>
2024-07-11 06:38:10 -0700

    some changes

---
+++ src/admin/app.tsx
@@ -0,0 +1,56 @@
+import AuthLogo from "./extensions/cdm_logo.png";
+import Favicon from "./extensions/cdm_logo.ico";
+
+export default {
+  config: {
+    auth:{
+      logo: AuthLogo
+    },
+    head: {
+      favicon: Favicon,
+    },
+    theme: {
+      colors: {
+        primary100: "#cdd9cc",
+        primary200: "#b5c7b4",
+        primary500: "#6e916d",
+        primary600: "#295e29",
+        primary700: "#0b4d10",
+        danger700: "#e23a2d",
+      },
+    },
+    tutorials: false,
+    notification: { release: false},
+    locales: [
+      // 'ar',
+      // 'fr',
+      // 'cs',
+      // 'de',
+      // 'dk',
+      // 'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+      // 'no',
+      // 'pl',
+      // 'pt-BR',
+      // 'pt',
+      // 'ru',
+      // 'sk',
+      // 'sv',
+      // 'th',
+      // 'tr',
+      // 'uk',
+      // 'vi',
+      // 'zh-Hans',
+      // 'zh',
+    ],
+  },
+  bootstrap(app: any) {
+    console.log(app);
+  },
+};
