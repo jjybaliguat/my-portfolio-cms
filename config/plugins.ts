74f9dcf96db6be714510c30832a33477e0dcbdcb
jjybaliguat <justinejeraldbaliguat@gmail.com>
2024-07-12 08:48:48 -0700

    some chnages

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,4 @@
-export default () => ({
+export default ({ env }) => ({
     slugify: {
         enabled: true,
         config: {
@@ -17,5 +17,20 @@ export default () => ({
   ckeditor: {
     enabled: true,
     resolve: "./src/plugins/strapi-plugin-ckeditor"
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
   },
 });
